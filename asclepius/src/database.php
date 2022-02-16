2b51875d02344ea229091db6b0c38ce5527a7aac
ejohnguia <35359631+ejohnguia@users.noreply.github.com>
2022-02-16 16:13:55 -0700

    add azure password

--- asclepius/src/database.php
+++ asclepius/src/database.php
@@ -1,7 +1,7 @@
 <?php
 // PHP Data Objects(PDO) Sample Code:
 try {
-    $conn = new PDO("sqlsrv:server = tcp:seng401-sv.database.windows.net,1433; Database = seng401-db", "njoy", "{your_password_here}");
+    $conn = new PDO("sqlsrv:server = tcp:seng401-sv.database.windows.net,1433; Database = seng401-db", "njoy", "QUO=?vGO_PhCZZ)OpoPy|*S:dxB#+T;73&8GIJ/R|.gfyvnN8'");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch (PDOException $e) {
@@ -10,7 +10,7 @@ catch (PDOException $e) {
 }
 
 // SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "njoy", "pwd" => "{your_password_here}", "Database" => "seng401-db", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$connectionInfo = array("UID" => "njoy", "pwd" => "QUO=?vGO_PhCZZ)OpoPy|*S:dxB#+T;73&8GIJ/R|.gfyvnN8'", "Database" => "seng401-db", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
 $serverName = "tcp:seng401-sv.database.windows.net,1433";
 $conn = sqlsrv_connect($serverName, $connectionInfo);
 ?>
