4c806ebdcd003243dcf167c442d1acd1395d4a89
Etienne <etienne.lagace@shaw.ca>
2022-03-24 11:25:11 -0600

    changed homepage route

--- asclepius/client/src/index.js
+++ asclepius/client/src/index.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { AuthProvider } from './context/AuthProvider';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { AuthProvider } from "./context/AuthProvider";
 
 ReactDOM.render(
   <AuthProvider>
     <App />
   </AuthProvider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
