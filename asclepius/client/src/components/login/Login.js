27857ece5d0807781b13698b133b3ee5aa3b4ec9
Girimer <girimer.singh@ucalgary.ca>
2022-03-26 11:35:35 -0600

    UI modifications

--- asclepius/client/src/components/login/Login.js
+++ asclepius/client/src/components/login/Login.js
@@ -98,7 +98,7 @@ const Login = (props) => {
                 />
               </div>
             </div>
-            <Button type="submit" variant="outlined" color="primary">
+            <Button type="submit" variant="outlined" color="primary" style={{ border: '2px solid #1E90FF', color: '#1E90FF'}}>
               Login
             </Button>
             <div>
