c8bf8107e0d2b1def30e6d394f38c73a2e72a8db
EtienneL321 <etienne.lagace@shaw.ca>
2022-03-26 19:48:47 -0600

    Changed patient items to buttons

--- asclepius/client/src/components/UI/Patients/PatientList.js
+++ asclepius/client/src/components/UI/Patients/PatientList.js
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import Button from '@material-ui/core/Button';
 import './PatientList.css';
+import { Divider } from '@material-ui/core';
 
 const PatientList = (props) => {
     const assignedPatientsInfo = props.assignedPatientsInfo;
+
+    const renderPatient = () => {
+        console.log("Rendered patient");
+    }
 
     return (
         <div>
@@ -11,10 +17,10 @@ const PatientList = (props) => {
                 {assignedPatientsInfo
                 .map((patient) => {
                     return (
-                        <div className='patientItem'>
+                        <button key={patient.patientID} className='patientItem' onClick={renderPatient}>
                             <h4>Patient ID: {patient.patientID}, </h4>
                             <h4>Name:{patient.patientName}</h4>
-                        </div>
+                        </button>
                     );
                 })}
                 
