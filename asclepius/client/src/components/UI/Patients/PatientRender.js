72d10ff9efb40a1984df6bb8845b086c8a0b3619
EtienneL321 <etienne.lagace@shaw.ca>
2022-03-26 23:17:32 -0600

    Updated admin patient view

--- asclepius/client/src/components/UI/Patients/PatientRender.js
+++ asclepius/client/src/components/UI/Patients/PatientRender.js
@@ -11,15 +11,12 @@ const PatientRender = (props) => {
     const renderPatient = (p) => {
         setPatientFile(true);
         patient.current = p;
-        console.log("Patient.current ", patient.current);
     }
 
     const MainComponentRender = () => {
         if (patientFile) {
-            console.log("Now Patient.current ", patient.current);
             return <Patient patientFile={patient.current} />;
         }
-        console.log(props.assignedPatientsInfo);
 
         return <PatientList 
                     renderPatient={renderPatient} 
