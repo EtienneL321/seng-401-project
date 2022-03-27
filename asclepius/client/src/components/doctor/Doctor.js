bda9a1ea21ded86e53e9001a3dac6ecc3df4edea
Etienne Lagace <63128108+EtienneL321@users.noreply.github.com>
2022-03-27 11:46:18 -0600

    Update Doctor.js
    Removed PatientList

--- asclepius/client/src/components/doctor/Doctor.js
+++ asclepius/client/src/components/doctor/Doctor.js
@@ -4,7 +4,6 @@ import NavBar from '../UI/NavBar';
 import useAuth from '../../hooks/useAuth';
 import '../home/HomePage.css';
 import '../CommonUser.css';
-import PatientList from '../UI/Patients/PatientList';
 import PatientRender from '../UI/Patients/PatientRender';
 
 const Doctor = (props) => {
