255e878a75fb2733741938a43d8259c8b7d61b99
EtienneL321 <etienne.lagace@shaw.ca>
2022-03-26 23:12:53 -0600

    Patient file rendering works!

--- asclepius/client/src/components/UI/Patients/PatientList.js
+++ asclepius/client/src/components/UI/Patients/PatientList.js
@@ -1,29 +1,26 @@
 import React, { useState, useEffect, useRef } from 'react';
-import Button from '@material-ui/core/Button';
 import './PatientList.css';
-import { Divider } from '@material-ui/core';
+import Patient from '../../patient/Patient';
+
 
 const PatientList = (props) => {
     const assignedPatientsInfo = props.assignedPatientsInfo;
 
-    const renderPatient = (p) => {
-        console.log("Rendered patient", p);
-    }
-
     return (
         <div>
-            <h1>{props.title ? props.title : "Assigned Patients"}</h1>
-            <div className='patient-list-container'>
-                {assignedPatientsInfo
-                .map((patient) => {
-                    return (
-                        <button key={patient.patientID} className='patientItem' onClick={() => renderPatient(patient)}>
-                            <h4>Patient ID: {patient.patientID}, </h4>
-                            <h4>Name:{patient.patientName}</h4>
-                        </button>
-                    );
-                })}
-                
+            <div>
+                <h1>{props.title ? props.title : "Assigned Patients"}</h1>
+                <div className='patient-list-container'>
+                    {assignedPatientsInfo
+                    .map((patient) => {
+                        return (
+                            <button key={patient.patientID} className='patientItem' onClick={() => props.renderPatient(patient)}>
+                                <h4>Patient ID: {patient.patientID}, </h4>
+                                <h4>Name:{patient.patientName}</h4>
+                            </button>
+                        );
+                    })}
+                </div>
             </div>
         </div>
     );
--- asclepius/client/src/components/patient/Patient.js
+++ asclepius/client/src/components/patient/Patient.js
@@ -1,28 +1,16 @@
 import React, { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
+import NavBar from '../UI/NavBar';
 
-const Patient = () => {
+
+const Patient = (props) => {
+
+    console.log("Patient is: ", props.patientFile);
 
     return (
         <div className='MainApp'>
-            <NavBar />
-            <div className='main-content-container'>
-                <div className='user-sidebar'>
-                    <h3>Hello, Doctor {staffInfo.Name}</h3>
-                    <div className='navigation-btns-user'>
-                        <button type="button" className='nav-btns'>
-                            View Patients
-                        </button>
-                        <button type="button" className='nav-btns'>
-                            Order Patient Medication
-                        </button>
-                        
-                    </div>
-                </div>
-                
-                <div className='main-page-content'>
-                    <h2>Hello World!</h2>
-                </div>
+            <div className='main-page-content'>
+                <h2>Hello World!</h2>
             </div>
         </div>
     )
