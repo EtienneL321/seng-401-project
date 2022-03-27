6da1bfa9d284b2d27d84aeb8cf2ebef3329ba026
Girimer <girimer.singh@ucalgary.ca>
2022-03-27 16:43:01 -0600

    added banner pic

--- asclepius/client/src/components/GreetingBanner.js
+++ asclepius/client/src/components/GreetingBanner.js
@@ -4,6 +4,7 @@ const GreetingBanner = () => {
     return (
     <div className='banner-container'>
         <div className='title-container'>
+            <img src="https://cdn.pixabay.com/photo/2014/03/25/16/34/rod-of-asclepius-297429_960_720.png" />
             <h1>Ascelpius</h1>
             <h3>Your patient and pharmaceuticals information system</h3>
         </div>
